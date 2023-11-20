d0486d66c20fbaeacb50de3ec1414681c2f94e41
stapia casa <santiago.tapia@upm.es>
2023-11-20 16:36:25 +0100

    change names

---
+++ include/as2_psdk/as2_psdk_node.hpp
@@ -0,0 +1,11 @@
+#ifndef AS2_NODE_AS2_PSDK_NODE_HPP_10982741902451
+#define AS2_NODE_AS2_PSDK_NODE_HPP_10982741902451
+
+#include <rclcpp/rclcpp.hpp>
+#include "as2_core/aerial_platform.hpp"
+
+class As2PsdkNode : public as2::AerialPlatform {
+public:
+};
+
+#endif
---
+++ src/as2_psdk_node.cpp
@@ -0,0 +1,1 @@
+#include "as2_psdk/as2_psdk_node.hpp"
